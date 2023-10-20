8576aa7210222bb327f2e1d911562f0c7ae93016
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:18:12 +0300

    relative paths

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from "@/components/Logo";
+
+const Header = ({}) => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
+// by Rokas with ❤️
---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = ({}) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Logo;
+// by Rokas with ❤️
