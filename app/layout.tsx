7161650ad6394506f4b408e0c1080cf8582427b2
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:36:48 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from "@/components/Header";
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from "@/components/Logo";
+import {DarkModeToggle} from "@/components/DarkModeToggle";
 
 const Header = ({}) => {
   return (
@@ -7,7 +8,7 @@ const Header = ({}) => {
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
         <div className="flex flex-1 items-center justify-end space-x-4">
-
+          <DarkModeToggle />
         </div>
       </nav>
     </header>
