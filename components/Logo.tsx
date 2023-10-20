17afa82db2b76b27b02ee15575ad80b2be4c8eaf
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:25:28 +0300

    fix

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Link from "next/link";
+import {AspectRatio} from "@/components/ui/aspect-ratio";
 
 const Logo = ({}) => {
   return (
-    <Link href={'/'}>
-      <h1 className="text-lg font-bold text-black">Chat With Anyone</h1>
+    <Link href={'/'} className="overflow-hidden" prefetch={false}>
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio ratio={16 / 9} className="flex items-center justify-center">
+          <h1 className="text-xl font-bold text-black dark:filter dark:invert">Chat With Anyone</h1>
+        </AspectRatio>
+      </div>
     </Link>
   );
 };
