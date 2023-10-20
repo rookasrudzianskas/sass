03cec73a1a3e06afcba2146a71665e9ac421ce7e
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:05:07 +0300

    session

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import {authOptions} from "@/auth";
 
 const Header = async ({}) => {
   const session = await getServerSession(authOptions);
+  console.log(session)
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
