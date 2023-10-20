9958e5c5a69b4b8e2cc6f34c94bede91126c10b8
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:02:03 +0300

    commit support

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from "@/components/Header";
 import { ThemeProvider } from '@/components/ThemeProvider';
+import ClientProviders from "@/components/ClientProviders";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,18 +18,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClientProviders>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClientProviders>
   )
 }
