77b53e102e14bfd746d34b87e1bcc206d7a5dfad
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:32:56 +0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,14 @@ import Logo from "@/components/Logo";
 
 const Header = ({}) => {
   return (
-    <div>
-      <Logo />
-    </div>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+
+        </div>
+      </nav>
+    </header>
   );
 };
 
