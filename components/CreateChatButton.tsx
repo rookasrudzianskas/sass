57324306cb3bdc74ceb083b6af26269176a1fbc4
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:24:52 +0300

    headerDown

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -3,8 +3,14 @@
 import React from 'react';
 import {Button} from "@/components/ui/button";
 import {MessageSquarePlusIcon, MessagesSquareIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
 
 const CreateChatButton = ({}) => {
+  const router = useRouter();
+  const createNewChat = async () => {
+    router.push('/chat/asb');
+  }
+
   return (
     <Button variant={'ghost'}>
       <MessageSquarePlusIcon className="" />
