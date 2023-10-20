6f729ab366c835ca8a7b8fdaada1479309717d00
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:40:01 +0300

    pro thing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from "@/components/Logo";
 import {DarkModeToggle} from "@/components/DarkModeToggle";
+import UserButton from "@/components/UserButton";
 
 const Header = ({}) => {
   return (
@@ -9,6 +10,7 @@ const Header = ({}) => {
         <Logo />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <DarkModeToggle />
+          <UserButton />
         </div>
       </nav>
     </header>
---
+++ components/UserButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+const UserButton = ({}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+  );
+};
+
+export default UserButton;
+// by Rokas with ❤️
