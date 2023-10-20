e32d6ea83d610c9e38898bd1236d59b6f7424610
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:04:37 +0300

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import Logo from "@/components/Logo";
 import {DarkModeToggle} from "@/components/DarkModeToggle";
 import UserButton from "@/components/UserButton";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/auth";
 
-const Header = ({}) => {
+const Header = async ({}) => {
+  const session = await getServerSession(authOptions);
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
-        <Logo />
+        <Logo/>
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <DarkModeToggle />
-          <UserButton />
+          <DarkModeToggle/>
+          <UserButton/>
         </div>
       </nav>
     </header>
