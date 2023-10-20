5bb79cdd84b4dc1c76a1cd32f91c1910d19f961c
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:08:21 +0300

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import {DarkModeToggle} from "@/components/DarkModeToggle";
 import UserButton from "@/components/UserButton";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/auth";
+import Link from "next/link";
+import {MessagesSquareIcon} from "lucide-react";
 
 const Header = async ({}) => {
   const session = await getServerSession(authOptions);
@@ -13,6 +15,19 @@ const Header = async ({}) => {
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo/>
         <div className="flex flex-1 items-center justify-end space-x-4">
+
+          {session ? (
+            <>
+              <Link href={'/chat'} prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
+            </>
+          ) : (
+            <Link href={'/pricing'}>
+              Pricing
+            </Link>
+          )}
+
           <DarkModeToggle/>
           <UserButton/>
         </div>
