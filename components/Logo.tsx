7905cebe7fca8221218389060b1bd03a88037e75
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 16:19:22 +0300

    rokas

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from "next/link";
 
 const Logo = ({}) => {
   return (
-    <div>
-      
-    </div>
+    <Link href={'/'}>
+      <h1 className="text-lg font-bold text-black">Chat With Anyone</h1>
+    </Link>
   );
 };
 
