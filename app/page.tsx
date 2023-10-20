a9ad7cccd18a4d7bb4dfe14ed01d16d39fb133cd
Rokas <rookas.rudzenskas@gmail.com>
2023-10-20 17:47:08 +0300

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
                   unoptimized={true}
-                  src={'/images/hero.png'}
+                  src={'https://i.ytimg.com/vi/_RO6Q1qhm0c/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLABkbMcse-DOfCGn10eMuiVwg9boQ'}
                   alt={'Hero'}
                   width={2432}
                   height={1442}
