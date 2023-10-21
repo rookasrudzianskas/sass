1b0fbfe047274e5da0712d95f95be1ebdbb4853f
Rokas <rookas.rudzenskas@gmail.com>
2023-10-21 11:25:51 +0300

    fix

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -2,13 +2,23 @@
 
 import React, {useEffect} from 'react';
 import {useSession} from "next-auth/react";
+import {subscriptionRef} from "@/lib/converters/Subscription";
+import {onSnapshot} from "@firebase/firestore";
 
 const SubscriptionProvider = ({}) => {
   const { data: session } = useSession();
 
   useEffect(() => {
-
-  }, []);
+    if(!session) return;
+    return onSnapshot(subscriptionRef(session?.user.id), (snapshot) => {
+      if(snapshot.empty) {
+        console.log('No subscription of the user');
+        return
+      } else {
+        console.log('User has subscription');
+      }
+    });
+  }, [session]);
 
   return (
     <div>
