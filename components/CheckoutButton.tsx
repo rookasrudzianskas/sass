2169b7747ac6dfbed0d36179f0d0ffef00ffb2c8
Rokas <rookas.rudzenskas@gmail.com>
2023-10-21 09:43:04 +0300

    fix

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const CheckoutButton = ({}) => {
   return (
-    <div className="bg-red-200 text-red-400">
-      I am Rokas
+    <div>
+      {/* if subscribed, show the user subscribed */}
+
+      <button className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
+        Sign Up
+      </button>
     </div>
   );
 };
