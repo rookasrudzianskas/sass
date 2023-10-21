17b604c864d28092112a1a0773f9d61fbcb05cd7
Rokas <rookas.rudzenskas@gmail.com>
2023-10-21 09:38:21 +0300

    fix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -16,7 +16,7 @@ const Register = async ({}) => {
         <div className="relative">
           <svg
             viewBox="0 0 1208 1024"
-            className="absolute-top-10 left-1/2-z-10 h-[64rem] -translate-x-1/2
+            className="absolute -top-10 left-1/2-z-10 h-[64rem] -translate-x-1/2
               [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12
                 md:-top-20 lg:-top-12 xl:top-0">
               <ellipse
