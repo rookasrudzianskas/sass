39aec4727a606ba506b84f57b758afc503989b57
Rokas <rookas.rudzenskas@gmail.com>
2023-10-21 09:45:19 +0300

    stripe

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import React from 'react';
 
 const CheckoutButton = ({}) => {
+  const createCheckoutSession = async () => {
+    // This is where stripe will create checkout session
+  }
+
   return (
-    <div>
+    <div className="flex flex-col space-y-2">
       {/* if subscribed, show the user subscribed */}
 
-      <button className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
+      <button onClick={() => createCheckoutSession()} className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
         Sign Up
       </button>
     </div>
