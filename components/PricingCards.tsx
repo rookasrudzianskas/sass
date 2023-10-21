711f4df3daaa00d67561b2b894f81d87c6e5ec33
Rokas <rookas.rudzenskas@gmail.com>
2023-10-21 09:12:32 +0300

    fix

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
+const tiers = [
+  {
+    name: "Starter",
+    id: null,
+    href: "#",
+    priceMonthly: null,
+    description: "Get chatting right away with anyone, anywhere!",
+    features: [
+      "20 messages chat limit in Chats",
+      "2 Participant limit in Chat",
+      "3 Chat Rooms limit",
+      "Supports 2 languages",
+      "48 hours support response time"
+    ],
+  },
+  {
+    name: "Pro",
+    id: "si_OnlcsLNQYbMvzV",
+    href: "#",
+    priceMonthly: "$5.99",
+    description: "Unlock the full potential with PRO!",
+    features: [
+      "Unlimited messages in Chats",
+      "Unlimited Participant limit in Chat",
+      "Unlimited Chat Rooms limit",
+      "Supports 10 languages",
+      "48 hours support response time",
+      "Multimedia support in chats (coming soon)",
+      "Early access to the new features"
+    ],
+  },
+]
+
 const PricingCards = ({}) => {
   return (
     <div>
-      
+
     </div>
   );
 };
